ca7bf73147c04f4d88e770b5bf2925cf996304ae
Lynne Jones <lynnej@uw.edu>
2019-01-02 12:49:32 -0800

    start adding object info, including oorb mags

---
+++ sso_tools/ztf/objectInfo.py
@@ -0,0 +1,6 @@
+"""Add information about a particular object - orbit, known lightcurve, and predicted magnitudes."""
+
+import numpy as np
+import pandas as pd
+import astroquery
+import lsst.sims.movingObjects as mo
